cf777c7285ec65315aceac510d5fdbae7ddb791b
Giulia Marcela <giuliamendes67@gmail.com>
2022-04-16 15:25:12 -0300

    feat(student): add initial student entity structure

---
+++ src/main/java/com/challenge/school/modules/student/Student.java
@@ -0,0 +1,23 @@
+package com.challenge.school.modules.student;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "students")
+public class Student {
+    @Id @GeneratedValue(generator = "uuid")
+    private UUID id;
+    private String enrollment;
+    private String name;
+    private String email;
+}
