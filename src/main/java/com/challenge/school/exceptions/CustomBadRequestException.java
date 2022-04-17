777ee7bdff5d71e766807365b16868c146af2c84
Giulia Marcela <giuliamendes67@gmail.com>
2022-04-16 21:56:38 -0300

    feat: add a custom bad request exception

---
+++ src/main/java/com/challenge/school/exceptions/CustomBadRequestException.java
@@ -0,0 +1,7 @@
+package com.challenge.school.exceptions;
+
+public class CustomBadRequestException extends RuntimeException {
+    public CustomBadRequestException(String message) {
+        super(message);
+    }
+}
