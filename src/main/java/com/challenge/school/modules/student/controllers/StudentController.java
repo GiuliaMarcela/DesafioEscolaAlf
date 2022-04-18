4b75c13ab97ee39d8fdfd717376d3588d27de1c0
Giulia Marcela <giuliamendes67@gmail.com>
2022-04-18 20:43:17 -0300

    feat(student): add documented endpoint to get student by enrollment

--- src/main/java/com/challenge/school/modules/student/controllers/StudentController.java
+++ src/main/java/com/challenge/school/modules/student/controllers/StudentController.java
@@ -2,19 +2,17 @@ package com.challenge.school.modules.student.controllers;
 
 import com.challenge.school.modules.student.dto.StudentRequest;
 import com.challenge.school.modules.student.dto.StudentResponse;
-
 import com.challenge.school.modules.student.usecases.CreateStudentUseCase;
+import com.challenge.school.modules.student.usecases.GetStudentByEnrollmentUseCase;
 
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @RestController
@@ -22,6 +20,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON;
 @RequestMapping("/${api.prefix}/${api.version}/students")
 public class StudentController implements StudentControllerDocs {
     private final CreateStudentUseCase createStudentUseCase;
+    private final GetStudentByEnrollmentUseCase getStudentByEnrollmentUseCase;
 
     @Override
     @PostMapping
@@ -30,6 +29,17 @@ public class StudentController implements StudentControllerDocs {
 
         return ResponseEntity
                 .status(CREATED)
+                .contentType(APPLICATION_JSON)
+                .body(response);
+    }
+
+    @Override
+    @GetMapping
+    public ResponseEntity<StudentResponse> handleGetByEnrollment(@RequestParam String enrollment) {
+        StudentResponse response = getStudentByEnrollmentUseCase.execute(enrollment);
+
+        return ResponseEntity
+                .status(OK)
                 .contentType(APPLICATION_JSON)
                 .body(response);
     }
