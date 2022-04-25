41d034cd04d6c6ccf3014a1f863e649bfe490acc
Giulia Marcela <giuliamendes67@gmail.com>
2022-04-25 19:24:09 -0300

    refactor: remove unused imports

--- src/test/java/com/challenge/school/modules/template/TemplateControllerTest.java
+++ src/test/java/com/challenge/school/modules/template/TemplateControllerTest.java
@@ -1,20 +1,20 @@
 package com.challenge.school.modules.template;
 
-import com.challenge.school.exceptions.CustomNotFoundException;
 import com.challenge.school.modules.template.builders.TemplateRequestBuilder;
 import com.challenge.school.modules.template.builders.TemplateResponseBuilder;
 import com.challenge.school.modules.template.controllers.TemplateController;
 import com.challenge.school.modules.template.dto.TemplateRequest;
 import com.challenge.school.modules.template.dto.TemplateResponse;
 import com.challenge.school.modules.template.usecases.CreateTemplateUseCase;
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.Mockito.when;
