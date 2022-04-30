e6ef72dd4c7252776de5cf62c242a4eef399f04a
Giulia Marcela <giuliamendes67@gmail.com>
2022-04-29 22:13:28 -0300

    feat: create a new method in GetExamByIdUseCase

--- src/main/java/com/challenge/school/modules/exam/usecases/GetExamByIdUseCase.java
+++ src/main/java/com/challenge/school/modules/exam/usecases/GetExamByIdUseCase.java
@@ -1,7 +1,9 @@
 package com.challenge.school.modules.exam.usecases;
 
 import com.challenge.school.exceptions.CustomNotFoundException;
+import com.challenge.school.modules.exam.ExamMapper;
 import com.challenge.school.modules.exam.ExamRepository;
+import com.challenge.school.modules.exam.dto.ExamResponse;
 import com.challenge.school.modules.exam.entities.Exam;
 
 import lombok.RequiredArgsConstructor;
@@ -16,8 +18,19 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class GetExamByIdUseCase {
     private final ExamRepository repository;
+    private final ExamMapper examMapper = ExamMapper.INSTANCE;
 
-    public Exam execute(String id) {
+    public ExamResponse execute(String id) {
+        String message = String.format("Não foi possível encontrar exame com o id %s", id);
+
+        Exam exam = repository
+                .findById(UUID.fromString(id))
+                .orElseThrow(() -> new CustomNotFoundException(message));
+
+        return examMapper.toExamResponse(exam);
+    }
+
+    public Exam search(String id) {
         String message = String.format("Não foi possível encontrar exame com o id %s", id);
 
         return repository
