48d921386fc653b4dedfa2bbceca08097602b6fb
Giulia Marcela <giuliamendes67@gmail.com>
2022-05-06 22:47:31 -0300

    test[student]: ensure findByEmail returns student if a valid email is provided

---
+++ src/test/java/com/challenge/school/modules/student/StudentRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.challenge.school.modules.student;
+
+import com.challenge.school.modules.student.builders.StudentBuilder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+class StudentRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    StudentRepository systemUnderTest;
+
+    StudentBuilder studentBuilder;
+
+    @BeforeEach
+    void setUp() {
+        studentBuilder = StudentBuilder.builder().build();
+    }
+
+    @Test
+    void findByEmailResultShouldReturnTrueWhenValidEmailIsPassed() {
+        Student student = studentBuilder.buildStudent();
+
+        entityManager.persist(student);
+
+        Optional<Student> result = systemUnderTest.findByEmail(student.getEmail());
+
+        assertEquals(true, result.isPresent());
+        assertThat(result.get().getEmail()).isEqualTo(student.getEmail());
+    }
+}
